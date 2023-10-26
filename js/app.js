7f888326331c0c0eec13d7cbc2e9c1ed824b737c
igorliotta <liottaigor@gmail.com>
2023-10-26 12:05:50 +0200

    Fix:

--- js/app.js
+++ js/app.js
@@ -3,31 +3,58 @@ const { createApp } = Vue;
 createApp({
     data() {
       return {
+        nexTaskText: '',
         headerTitle: 'Vue To Do List',
         todos: [
             {
-                text: 'Lavare i piatti',
-                done: false,
+                text: 'Fare i compiti',
+                done: false
             },
             {
                 text: 'Fare la spesa',
-                done: true,
+                done: true
             },
             {
-                text: 'Sistemare il lavandino',
-                done: false,
-            },
-            {
-                text: 'Comprare un regalo per la ragazza',
-                done: true,
+                text: 'Fare il bucato',
+                done: false
             },
             
-        ]
+            
+        ],
 
         }
     },
     methods: {
-        
+        removeTask(i) {
+            this.todos.splice(i,1);
+        },
+        addTask() {
+
+            const text = this.nexTaskText.trim();
+
+            if (text !== '') {
+                const newTask = {
+                    text: text,
+                    done: false,
+                }
+    
+                this.todos.push(newTask);
+            } 
+
+            this.nexTaskText = ''
+            
+        },
+        toogleTask(task) {
+            
+            if (task.done === true) {
+                task.done = false
+            } else {
+                task.done = true
+            }
+
+            // Stessa cosa si potrebbe fare con una sola operazione:
+            // task.done = !task.done
+        }
     },
     mounted() {
         console.log("VUE OK");
